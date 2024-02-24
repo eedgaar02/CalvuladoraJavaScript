2cce7ed4042f73a889925ca83218b129fba231b8
Edgar <eedgaarlopez02@gmail.com>
2024-02-24 18:15:39 +0100

    Comentarios javascript

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
-const pantalla = document.getElementById("pantalla");
-const botones = document.querySelectorAll("button");
+const pantalla = document.getElementById("pantalla"); //constante que obtiene el elemento con id pantalla
+const botones = document.querySelectorAll("button"); //constante que almacen todos los botones del documento
 
+//for eacho con evento listener click
 botones.forEach(boton =>{
     boton.addEventListener("click",()=>{
-        const botonApretado = boton.textContent;
+        const botonApretado = boton.textContent; //constante almacena el contenido de cada boton
 
+        //Condicional tecla borrar
         if(boton.id === "borrar"){
             pantalla.textContent = "0";
             return;
         }
+        //Condicional tecla retroceso
         if(boton.id === "retroceso"){
             if(pantalla.textContent.length === 1 || pantalla.textContent === "Error"){
                 pantalla.textContent = "0";
@@ -17,6 +20,7 @@ botones.forEach(boton =>{
             }
             return;
         }
+        //Condicional tecla igual
         if(boton.id === "="){
             try{
                 pantalla.textContent = eval(pantalla.textContent);
@@ -25,10 +29,12 @@ botones.forEach(boton =>{
             }
             return;
         }
+        //Condicional control de salida por pantalla
         if(pantalla.textContent === "0" || pantalla.textContent === "Error"){
             pantalla.textContent = botonApretado;
         }else{
             pantalla.textContent += botonApretado
         }
     })
+    
 })
